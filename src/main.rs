8f8e2ecc99029cc88aba87fb91862f91b8c059aa
jonaseriksson84 <jonaseriksson84@gmail.com>
2023-03-22 23:26:28 +0100

    interactive prompt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use chrono::prelude::*;
-use clap::Parser;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
+use std::{fs::OpenOptions, io::Write};
 
-#[derive(Parser, Debug)]
-struct Opts {
-    #[clap(short, long)]
-    activity: String,
-}
+use chrono::Local;
 
 fn main() {
-    let opts = Opts::parse();
-    let activity = opts.activity;
+    let mut exit = false;
+
+    while !exit {
+        println!("Choose an option:");
+        println!("1. Log activity");
+        println!("2. View activities");
+        println!("3. Edit activity");
+        println!("4. Delete activity");
+        println!("5. Exit");
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let choice: u8 = input.trim().parse().unwrap_or(0);
+
+        match choice {
+            1 => log_activity(),
+            2 => view_activities(),
+            3 => edit_activity(),
+            4 => delete_activity(),
+            5 => {
+                println!("Exiting...");
+                exit = true;
+            }
+            _ => println!("Invalid option, please try again."),
+        }
+    }
+}
+
+fn log_activity() {
+    // Prompt the user for activity input
+    println!("Enter the activity:");
+    let mut activity = String::new();
+    std::io::stdin().read_line(&mut activity).unwrap();
+    activity = activity.trim().to_string();
 
     let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     let log_entry = format!("{}: {}\n", timestamp, activity);
@@ -27,4 +52,16 @@ fn main() {
     } else {
         println!("Activity logged successfully!");
     }
+}
+
+fn view_activities() {
+    // TODO: Implement view_activities
+}
+
+fn edit_activity() {
+    // TODO: Implement edit_activity
+}
+
+fn delete_activity() {
+    // TODO: Implement delete_activity
 }
